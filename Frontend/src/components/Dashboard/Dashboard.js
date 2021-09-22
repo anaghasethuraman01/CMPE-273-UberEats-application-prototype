7044baf7a59de3ec70278a6bab1f4577334a5c93
Anagha Sethuraman <anagha.sethuraman@sjsu.edu>
2021-09-22 02:09:49 -0700

    Main Dashboard

--- Frontend/src/components/Dashboard/Dashboard.js
+++ Frontend/src/components/Dashboard/Dashboard.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import Login from '../Login/Login';
 class Dashboard extends Component{
     render() {
         return (
             <div className="headeritems" >
-                
-                <Link className="link" to='/register' >Sign Up</Link>
-                <Link className="link" to='/login'>Sign In</Link>        
+                <Login/>
+
+                {/* <Link className="link" to='/register' >Sign Up</Link>
+                <Link className="link" to='/login'>Sign In</Link>         */}
                
             </div>
         )
