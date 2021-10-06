f8789c8418d7d38cdcfe544950032701d0e376c9
Anagha Sethuraman <anagha.sethuraman@sjsu.edu>
2021-10-06 10:56:17 -0700

    Images

---
+++ Frontend/.eslintrc.js
@@ -0,0 +1,32 @@
+{
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "parser": "@babel/eslint-parser",
+    "extends": [
+        "plugin:react/recommended",
+        "airbnb"
+    ],
+    "parserOptions": {
+        "parser": "@babel/eslint-parser",
+        "requireConfigFile": false,
+        "babelOptions": {
+            "presets": ["@babel/preset-react"]
+         },
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        
+    }
+}
+// module.exports = {
+//   "parser": "babel-eslint",
+// };
